76414011e6b9fd0ea83d55ee4180b74c3860ed09
subin <subindevkdeivendran@gmail.com>
2021-06-21 12:55:25 +0530

    new: Add Amazon-In spider
    Update README

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Crawler
 A Web crawler, using [Scrapy](https://scrapy.org/) and [Selectorlib](https://pypi.org/project/selectorlib/).
+
+## To start crawling Amazon.in
+Currently supporting static url alone. Add or Modify the search urls [here](https://github.com/subin1011/Crawler/blob/master/crawler/spiders/AmazonIn.py#L12)
+Command to start crawling Amazon Product lists and details
+```bash
+cd <path/to/the/crawler/project>
+scrapy crawl AmazonIn
+```
---
+++ crawler/spiders/AmazonIn.py
@@ -0,0 +1,45 @@
+import scrapy
+import selectorlib
+import os
+import json
+import urllib.parse
+
+
+class AmazonInSpider(scrapy.Spider):
+    name = 'AmazonIn'
+    base_url = 'https://www.amazon.in/'
+    allowed_domains = ['amazon.in']
+    start_urls = ['https://www.amazon.in/s?k=snacks']
+
+    # Create Extractor for listing page
+    listing_page_extractor = selectorlib.Extractor.from_yaml_file(os.path.join(os.path.dirname(__file__),'../selectorlib_yaml/amazon_in/ListingPage.yml'))
+    # Create Extractor for product page
+    product_page_extractor = selectorlib.Extractor.from_yaml_file(os.path.join(os.path.dirname(__file__),'../selectorlib_yaml/amazon_in/ProductPage.yml'))
+
+    # List page output file
+    listing_page_output_file = os.path.join(os.path.dirname(__file__),'../output/amazon_in/ListingPageOutput.jsonl')
+    # Product page output file
+    product_page_output_file = os.path.join(os.path.dirname(__file__),'../output/amazon_in/ProductPageOutput.jsonl')
+
+    def parse(self, response):
+        # Extract data using Extractor
+        data = self.listing_page_extractor.extract(response.text)
+        self.write_to_file(self.listing_page_output_file, data)
+        if 'next_page' in data:
+            yield scrapy.Request(data['next_page'], callback=self.parse)
+        for p in data['products']:
+            url = urllib.parse.urljoin(self.base_url, p['url'])
+            yield scrapy.Request(url, callback=self.parse_product)
+
+    def parse_product(self, response):
+        # Extract data using Extractor
+        product = self.product_page_extractor.extract(response.text)
+        if product:
+            self.write_to_file(self.product_page_output_file, product)
+            yield product
+
+    def write_to_file(self, file, data):
+        with open(file,'a') as outfile:
+            if data:
+                json.dump(data, outfile)
+                outfile.write("\n")
