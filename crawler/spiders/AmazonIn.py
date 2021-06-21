b4b3a235d8720b9272954963437e168d543158b0
subin <subindevkdeivendran@gmail.com>
2021-06-21 13:30:37 +0530

    Update amazon-in spider to accept static query instead of static url

--- crawler/spiders/AmazonIn.py
+++ crawler/spiders/AmazonIn.py
@@ -2,14 +2,15 @@ import scrapy
 import selectorlib
 import os
 import json
-import urllib.parse
+from urllib.parse import urlencode
+from urllib.parse import urljoin
 
 
 class AmazonInSpider(scrapy.Spider):
     name = 'AmazonIn'
     base_url = 'https://www.amazon.in/'
     allowed_domains = ['amazon.in']
-    start_urls = ['https://www.amazon.in/s?k=snacks']
+    queries = ['snacks']
 
     # Create Extractor for listing page
     listing_page_extractor = selectorlib.Extractor.from_yaml_file(os.path.join(os.path.dirname(__file__),'../selectorlib_yaml/amazon_in/ListingPage.yml'))
@@ -21,6 +22,11 @@ class AmazonInSpider(scrapy.Spider):
     # Product page output file
     product_page_output_file = os.path.join(os.path.dirname(__file__),'../output/amazon_in/ProductPageOutput.jsonl')
 
+    def start_requests(self):
+        for query in self.queries:
+            url = 'https://www.amazon.in/s?' + urlencode({'k': query})
+            yield scrapy.Request(url=url, callback=self.parse)
+
     def parse(self, response):
         # Extract data using Extractor
         data = self.listing_page_extractor.extract(response.text)
@@ -28,7 +34,7 @@ class AmazonInSpider(scrapy.Spider):
         if 'next_page' in data:
             yield scrapy.Request(data['next_page'], callback=self.parse)
         for p in data['products']:
-            url = urllib.parse.urljoin(self.base_url, p['url'])
+            url = urljoin(self.base_url, p['url'])
             yield scrapy.Request(url, callback=self.parse_product)
 
     def parse_product(self, response):
